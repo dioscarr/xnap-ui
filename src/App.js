552dcd66ddd110bf78546ad0c6f53ef5abe84133
Dioscar <dioscarr@gmail.com>
2023-01-19 23:23:49 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -14,19 +14,103 @@ function App() {
   const [selectedCategory, setChildAliases] = useState([]);
   const [selectedParent, setSelectedParent] = useState("");
   const [selectcat,setSelectedCat] = useState("");
+  const [performane,setPerformance]= useState("");
+  const JobCompleted = []
+  const StartJob = async()=>{
+      debugger;
+      const c = parentAliases.filter(cat=>cat === "lawyers").map((cat, i) => {
+       const b = data
+        .filter((item) => item.parent_aliases[0] === cat)
+        .map((item1) => { 
+          
+         // State.getStatesOfCountry("US").map((state) => 
+         // {
+           const a = City.getCitiesOfState("US",selectedState).map((city) =>
+          //  City.getCitiesOfState("US", state.isoCode).map((city) =>
+            {
+              
+             // const cs = `${city.name} ${state.isoCode}`;
+              const cs = `${city.name} ${selectedState}`;
+              const catid = `${item1.alias}`;
+              
+              return `https://xnap1.onrender.com/BusinessSearch?location=${cs}&category=${catid}`;
+              
+              //  });
+            });
+            return a;
+          });
+          return b
+        });
+        console.log(c);
+        var d = 0;
+      //  for(var i=0;i<c.length -1; i++)
+      //  {
+        //  var s = c[i][d].length;
+         // var refreshId = setInterval(async function() {
+          //  d=d+1;
 
+          var ii = Math.floor(Math.random() * 10);
+            await callUrls(c[Math.floor(Math.random() * 10)][Math.floor(Math.random() * 10)].slice(ii,ii+3)) 
+          //  if ( d> s) {
+          //    clearInterval(refreshId);
+         //   }
+        //  }, 1000);
+
+
+          //const response = await axios.get(`https://xnap1.onrender.com/BusinessSearch?location=${cs}&category=${catid}`)
+        //}
+  }
+  
+  
+async function callUrls(urls) {
+  const batchSize = 20; // Number of requests per batch
+  const delay = 3000; // Delay between batches in milliseconds
+  const requests = []; // Array to store requests
+  let i = 0;
+  while (i < urls.length) {
+    requests.push(axios.get(urls[i]));
+    if (requests.length === batchSize || i === urls.length - 1) {
+      try {
+        // Wait for all requests in the batch to resolve
+        const results = await Promise.all(requests);
+        results.forEach(result => console.log(result.data));
+        // Clear the array of requests
+        requests.length = 0;
+        // Wait for the delay before making the next batch of requests
+        await new Promise(resolve => setTimeout(resolve, delay));
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    i++;
+  }
+}
+
+  useEffect(()=>{
+    
+    console.log(JobCompleted);
+  },[JobCompleted])
   useEffect(() => {
    try {
       axios
       // .get("https://xnap1.onrender.com/yelpcats")
         .get("https://xnap1.onrender.com/yelpcats")
         .then((response) => {
+          setPerformance(response.data.response_time);
+          setParentAliases(response.data.map((item) => item.parent_aliases[0]));
+        })
+        .catch((err) => console.log(err));
+      axios
+      .get("https://xnap1.onrender.com/yelpcats")
+       
+        .then((response) => {
           setData(response.data);
           setParentAliases(response.data.map((item) => item.parent_aliases[0]));
         })
         .catch((err) => console.log(err));
 
-      axios.get("https://xnap1.onrender.com/Leads").then((res) => {
+        axios.get("https://xnap1.onrender.com/Leads?skip=0&limit=2000").then((res) => {
+        //axios.get("http://localhost:3001/Leads?skip=0&limit=2000").then((res) => {
         const leads = res.data;
         console.log(leads);
         SetLeads(leads?.sort()?.reverse());
@@ -37,6 +121,7 @@ function App() {
   }, []);
 
   useEffect(() => {
+
     if (selectedParent) {
    
       setChildAliases(
@@ -60,21 +145,23 @@ function App() {
     </option>
   ));
   const addone = () => {
-console.log(selectcat)
 
-     axios.get(`https://xnap1.onrender.com/BusinessSearch?location=${selectedCity} ${selectedState}&category=${selectcat}`).then((res) => {
-   // axios.get(`http://localhost:3001?location=${selectedCity} ${selectedState}&category=${selectcat}`).then((res) => {
+
+  StartJob();
+
+    //   axios.get(`https://xnap1.onrender.com/BusinessSearch?location=${selectedCity} ${selectedState}&category=${selectcat}`).then((res) => {
+    // //axios.get(`http://localhost:3001/BusinessSearch?location=${selectedCity} ${selectedState}&category=${selectcat}`).then((res) => {
       
-    console.log(res.data);
-    alert(JSON.stringify(res.data));
+    // console.log(res.data);
+    // alert(JSON.stringify(res.data));
     
-    axios.get("https://xnap1.onrender.com/Leads").then((res) => {
-    //axios.get("http://localhost:3001/Leads").then((res) => {
-        const leads = res.data;
-        console.log(leads);
-        SetLeads(leads?.sort()?.reverse());
-      }); 
-    });
+    // //axios.get("https://xnap1.onrender.com/Leads").then((res) => {
+    // axios.get("http://localhost:3001/Leads?skip=0&limit=2000").then((res) => {
+    //     const leads = res.data;
+    //     console.log(leads);
+    //     SetLeads(leads?.sort()?.reverse());
+    //   }); 
+    // });
   };
 
   const handleDelete = (id) => {
@@ -91,9 +178,11 @@ console.log(selectcat)
   };
 
   return (
-    <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
-      <thead>
-
+<table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+  <thead>
+<tr>
+  <th colSpan={9}>performane: {performane}</th>
+  </tr>
         <tr>
           <th colSpan={2}>
             <div>
@@ -165,6 +254,7 @@ console.log(selectcat)
         <tr>
           <th className="mdl-data-table__cell--non-numeric">Name</th>
           <th className="mdl-data-table__cell--non-numeric">Url</th>
+          <th className="mdl-data-table__cell--non-numeric">Email(s)</th>
           <th className="mdl-data-table__cell--non-numeric">categories</th>
           <th className="mdl-data-table__cell--non-numeric">City State</th>
           <th className="mdl-data-table__cell--non-numeric">zip</th>
@@ -182,6 +272,11 @@ console.log(selectcat)
             <td className="mdl-data-table__cell--non-numeric">
               <a href={lead.xurl} target="_blank">
                 {lead.xurl}
+              </a>
+            </td>
+            <td className="mdl-data-table__cell--non-numeric">
+              <a href={lead.xurl} target="_blank">
+                {lead.emails[0]?.emails?.join()??""}
               </a>
             </td>
             <td className="mdl-data-table__cell--non-numeric">
